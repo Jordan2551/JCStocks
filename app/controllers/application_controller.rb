4eb0f7170470dea8e721924eee1a280d8a954ec9
Jordan Cohen <jordan1158@gmail.com>
2017-08-20 01:38:28 -0400

    Added complete authentication syste. Devise, bootstrap and appropriate views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #Before any action by this controller is executed, we MUST have a logged in user.
+  before_action :authenticate_user!
+
+
 end
